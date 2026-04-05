2065a41dd58dff475c0743081700db65ed2474e0
bin2003-VietNam <nguyenvanbin.9a10@gmail.com>
2026-04-05 18:56:56 +0700

    change categoryMennu

--- front_end/src/components/CategoryMenu.tsx
+++ front_end/src/components/CategoryMenu.tsx
@@ -1,6 +1,24 @@
 import React from 'react'
 
+interface MenuItem {
+  id: string;
+  label: string;          
+  url: string;                     
+  children?: MenuItem[];  
+}
+interface MenuColumn {
+  id: string;
+  title: string;          
+  items: MenuItem[];      
+}
 
+interface CategoryMenuProps {
+  id: string;
+  label: string;          
+  icon: string;           
+  url: string;
+  columns?: MenuColumn[]; 
+}
 
 function CategoryMenu() {
   return (
